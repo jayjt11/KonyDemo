078169fc7eda156ec9404756354dd2873c7e11d9
Jayant Tiwari <jayant.tiwari@rinira.com>
2022-05-27 22:57:28 +0530

    initial commit

---
+++ KonyiOSDemo/modules/listener.js
@@ -0,0 +1,8 @@
+//Type your code here
+
+function launchSDK() {
+  //Creates an object of class 'ZelleClass'
+var ZelleClassObject = new TestZelle.ZelleClass();
+//Invokes method 'loadSDK' on the object
+ZelleClassObject.loadSDK();
+}
---
+++ KonyiOSDemo/studioactions/mobile/AS_Button_ae7a6fa7040643118d5bab4533324913.js
@@ -0,0 +1,3 @@
+function AS_Button_ae7a6fa7040643118d5bab4533324913(eventobject) {
+    return launchSDK.call(this);
+}
